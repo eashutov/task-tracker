85430a9fb31e5885f35dad45691a676bd8691a2b
eashutov <egorkamaxwell@yandex.ru>
2024-05-24 00:52:37 +0600

    added common EventInfoImpl instead of separate for every entity

--- src/main/java/ru/shutov/itone/tasktracker/exception/event/EventInfoImpl.java
+++ src/main/java/ru/shutov/itone/tasktracker/exception/event/EventInfoImpl.java
@@ -1,14 +1,13 @@
 package ru.shutov.itone.tasktracker.exception.event;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.slf4j.event.Level;
 import org.springframework.http.HttpStatus;
 
-@Getter
 @AllArgsConstructor
-public enum ColEvent implements EventInfo {
-    COL_NOT_FOUND(HttpStatus.NOT_FOUND, Level.INFO);
+public enum EventInfoImpl implements EventInfo {
+    NOT_FOUND(HttpStatus.NOT_FOUND, Level.DEBUG),
+    ALREADY_EXISTS(HttpStatus.BAD_REQUEST, Level.DEBUG);
 
     private final HttpStatus httpStatus;
     private final Level level;
@@ -16,5 +15,10 @@ public enum ColEvent implements EventInfo {
     @Override
     public HttpStatus getStatus() {
         return httpStatus;
+    }
+
+    @Override
+    public Level getLevel() {
+        return level;
     }
 }
