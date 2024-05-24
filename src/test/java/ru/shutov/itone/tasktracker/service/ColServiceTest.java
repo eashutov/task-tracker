78918162f9a998c6b8442f8ebcb764c8ad783d16
eashutov <egorkamaxwell@yandex.ru>
2024-05-24 14:00:21 +0600

    added unit tests

---
+++ src/test/java/ru/shutov/itone/tasktracker/service/ColServiceTest.java
@@ -0,0 +1,136 @@
+package ru.shutov.itone.tasktracker.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.event.Level;
+import org.springframework.http.HttpStatus;
+import ru.shutov.itone.tasktracker.dto.post.ColPostDto;
+import ru.shutov.itone.tasktracker.dto.request.ColNameRequest;
+import ru.shutov.itone.tasktracker.dto.request.ColPositionRequest;
+import ru.shutov.itone.tasktracker.entity.Col;
+import ru.shutov.itone.tasktracker.entity.Desk;
+import ru.shutov.itone.tasktracker.exception.BusinessException;
+import ru.shutov.itone.tasktracker.mapper.ColMapper;
+import ru.shutov.itone.tasktracker.repository.ColRepository;
+import ru.shutov.itone.tasktracker.repository.DeskRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ColServiceTest {
+    @InjectMocks
+    ColService colService;
+
+    @Mock
+    ColRepository colRepository;
+
+    @Mock
+    ColMapper colMapper;
+
+    @Mock
+    DeskRepository deskRepository;
+
+    @Test
+    void create_shouldCreateNewCol() {
+        UUID deskId = UUID.randomUUID();
+        ColPostDto dto = new ColPostDto();
+        Col col = new Col();
+        Desk desk = new Desk();
+
+        when(colMapper.toModel(dto)).thenReturn(col);
+        when(deskRepository.getReferenceById(deskId)).thenReturn(desk);
+
+        colService.create(deskId, dto);
+
+        verify(colRepository, times(1)).save(col);
+        verify(deskRepository, times(1)).getReferenceById(deskId);
+        verifyNoMoreInteractions(colRepository);
+        verifyNoMoreInteractions(deskRepository);
+    }
+
+    @Test
+    void updateName_shouldUpdateName() {
+        UUID id = UUID.randomUUID();
+        Col col = new Col();
+        ColNameRequest request = new ColNameRequest("Name");
+
+        when(colRepository.findById(id)).thenReturn(Optional.of(col));
+
+        colService.updateName(id, request);
+
+        verify(colRepository, times(1)).findById(id);
+        verifyNoMoreInteractions(colRepository);
+    }
+
+    @Test
+    void updateName_shouldThrowBusinessException_ColNotFound() {
+        UUID id = UUID.randomUUID();
+        ColNameRequest request = new ColNameRequest("Name");
+
+        when(colRepository.findById(id)).thenReturn(Optional.empty());
+
+        BusinessException exception = assertThrows(BusinessException.class,
+                () -> colService.updateName(id, request));
+        assertEquals(Level.DEBUG, exception.getEventInfo().getLevel());
+        assertEquals(HttpStatus.NOT_FOUND, exception.getEventInfo().getStatus());
+
+        verify(colRepository, times(1)).findById(id);
+        verifyNoMoreInteractions(colRepository);
+    }
+
+    @Test
+    void delete_shouldDeleteCol() {
+        UUID id = UUID.randomUUID();
+
+        colService.delete(id);
+
+        verify(colRepository, times(1)).deleteById(id);
+        verifyNoMoreInteractions(colRepository);
+    }
+
+    @Test
+    void updatePosition_shouldUpdateSwapColPositions() {
+        UUID id = UUID.randomUUID();
+        int position = 1;
+        ColPositionRequest request = new ColPositionRequest(position);
+        Desk desk = new Desk();
+        Col col = new Col();
+        col.setPosition(0);
+        col.setDesk(desk);
+        Col colWithPosition = new Col();
+        colWithPosition.setPosition(position);
+
+        when(colRepository.findById(id)).thenReturn(Optional.of(col));
+        when(colRepository.findColByDeskAndPosition(desk, position)).thenReturn(Optional.of(colWithPosition));
+
+        colService.updatePosition(id, request);
+
+        verify(colRepository, times(1)).findById(id);
+        verify(colRepository, times(1)).findColByDeskAndPosition(desk, position);
+        verifyNoMoreInteractions(colRepository);
+    }
+
+    @Test
+    void updatePosition_shouldThrowBusinessException_ColNotFound() {
+        UUID id = UUID.randomUUID();
+        ColPositionRequest request = new ColPositionRequest(2);
+
+        when(colRepository.findById(id)).thenReturn(Optional.empty());
+
+        BusinessException exception  = assertThrows(BusinessException.class,
+                () -> colService.updatePosition(id, request));
+        assertEquals(Level.DEBUG, exception.getEventInfo().getLevel());
+        assertEquals(HttpStatus.NOT_FOUND, exception.getEventInfo().getStatus());
+
+        verify(colRepository, times(1)).findById(id);
+        verifyNoMoreInteractions(colRepository);
+    }
+}
---
+++ src/test/java/ru/shutov/itone/tasktracker/service/DeskServiceTest.java
@@ -0,0 +1,131 @@
+package ru.shutov.itone.tasktracker.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.event.Level;
+import org.springframework.http.HttpStatus;
+import ru.shutov.itone.tasktracker.dto.get.CompleteDeskDto;
+import ru.shutov.itone.tasktracker.dto.get.DeskDto;
+import ru.shutov.itone.tasktracker.dto.get.UserDto;
+import ru.shutov.itone.tasktracker.dto.post.DeskPostDto;
+import ru.shutov.itone.tasktracker.entity.Desk;
+import ru.shutov.itone.tasktracker.entity.User;
+import ru.shutov.itone.tasktracker.exception.BusinessException;
+import ru.shutov.itone.tasktracker.mapper.DeskMapper;
+import ru.shutov.itone.tasktracker.repository.DeskRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class DeskServiceTest {
+
+    @InjectMocks
+    DeskService deskService;
+
+    @Mock
+    DeskRepository deskRepository;
+
+    @Mock
+    DeskMapper deskMapper;
+
+    @Test
+    void findAll_shouldReturnAllDesks() {
+        User kenny = new User();
+        UserDto kennyDto = new UserDto();
+        kennyDto.setUsername("kenny");
+        User tony = new User();
+        UserDto tonyDto = new UserDto();
+        tonyDto.setUsername("tony");
+        List<Desk> desks = List.of(
+                Desk.builder()
+                        .creator(kenny)
+                        .build(),
+                Desk.builder()
+                        .creator(tony)
+                        .build()
+        );
+        DeskDto deskDtoKenny = new DeskDto();
+        deskDtoKenny.setCreator(kennyDto);
+        DeskDto deskDtoTony = new DeskDto();
+        deskDtoTony.setCreator(tonyDto);
+        List<DeskDto> expected = List.of(deskDtoKenny, deskDtoTony);
+
+        when(deskRepository.findAll()).thenReturn(desks);
+        when(deskMapper.toDto(desks)).thenReturn(expected);
+
+        List<DeskDto> actual = deskService.findAll();
+
+        assertNotNull(actual);
+        assertIterableEquals(expected, actual);
+        assertEquals(2, actual.size());
+
+        verify(deskRepository, times(1)).findAll();
+        verifyNoMoreInteractions(deskRepository);
+    }
+
+    @Test
+    void create_shouldCreateTask() {
+        DeskPostDto dto = new DeskPostDto();
+        Desk desk = new Desk();
+
+        when(deskMapper.toModel(dto)).thenReturn(desk);
+
+        deskService.create(dto);
+
+        verify(deskRepository, times(1)).save(desk);
+        verifyNoMoreInteractions(deskRepository);
+    }
+
+    @Test
+    void delete_shouldDeleteTask() {
+        UUID id = UUID.randomUUID();
+
+        deskService.deleteById(id);
+
+        verify(deskRepository, times(1)).deleteById(id);
+        verifyNoMoreInteractions(deskRepository);
+    }
+
+    @Test
+    void findById_shouldReturnCompleteDeskDto() {
+        UUID id = UUID.randomUUID();
+        Desk desk = new Desk();
+        String deskName = "Desk name";
+        CompleteDeskDto expected = new CompleteDeskDto();
+        expected.setName(deskName);
+
+        when(deskRepository.findById(id)).thenReturn(Optional.of(desk));
+        when(deskMapper.toCompleteDto(desk)).thenReturn(expected);
+
+        CompleteDeskDto actual = deskService.findById(id);
+        assertEquals(expected.getName(), actual.getName());
+
+        verify(deskRepository, times(1)).findById(id);
+        verifyNoMoreInteractions(deskRepository);
+    }
+
+    @Test
+    void findById_shouldThrowBusinessException_DeskNotFound() {
+        UUID id = UUID.randomUUID();
+
+        when(deskRepository.findById(id)).thenReturn(Optional.empty());
+
+        BusinessException exception  = assertThrows(BusinessException.class,
+                () -> deskService.findById(id));
+        assertEquals(Level.DEBUG, exception.getEventInfo().getLevel());
+        assertEquals(HttpStatus.NOT_FOUND, exception.getEventInfo().getStatus());
+
+        verify(deskRepository, times(1)).findById(id);
+        verifyNoMoreInteractions(deskRepository);
+        verifyNoInteractions(deskMapper);
+    }
+
+}
